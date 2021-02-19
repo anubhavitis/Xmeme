98cff4aa9cd0fa11a512045c9e350b504a608fb9
anubhavitis <ss.lfsgd@gmail.com>
2021-02-20 03:08:27 +0530

    Testing 105

--- install.sh
+++ install.sh
@@ -1,7 +1,2 @@
-set -e -x
-
-# Install go toolchain
-sudo wget -c https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvzf go1.15.2.linux-amd64.tar.gz
-
-export PATH=$PATH:/usr/local/go/bin
+sudo wget -c https://golang.org/dl/go1.16.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvzf go1.16.linux-amd64.tar.gz
--- server_run.sh
+++ server_run.sh
@@ -1,5 +1,11 @@
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go;
+export PATH=$PATH:$GOPATH/bin;
+echo $PATH
+echo $GOPATH
+
+go mod init github.com/anubhavitis/Xmeme
 go mod vendor
-
 go build 
-
 go run main.go
