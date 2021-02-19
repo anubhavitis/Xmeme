670845805cd1f8df9df1ff6cb59a42c6b28a05b5
anubhavitis <ss.lfsgd@gmail.com>
2021-02-20 00:11:09 +0530

    testing 101

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ set -e -x
 
 # Install go toolchain
 sudo wget -c https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvzf go1.15.2.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
